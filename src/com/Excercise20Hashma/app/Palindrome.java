fec36b2680150fc23fdcc3115c12aeb83aed2579
akain88 <larahaze88@gmail.com>
2019-10-23 13:08:23 -0500

    gave an example

--- src/com/Excercise20Hashma/app/Palindrome.java
+++ src/com/Excercise20Hashma/app/Palindrome.java
@@ -1,9 +1,14 @@
 package com.Excercise20Hashma.app;
-
+import com.Excercise20Hashma.model.StringAplications;
 public class Palindrome {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+	String cadPalindrome1="Anita lava la tina.";
+	String cadPalindrome2="YO dono rosas,oro no doy!!!";
+	
+	System.out.println(StringAplications.isPallindrome(cadPalindrome1));
+	System.out.println(StringAplications.isPallindrome(cadPalindrome2));
 
 	}
 
